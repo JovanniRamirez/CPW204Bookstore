e5e1b55f6a19067d9a1647a1a3f74da9f7a9a21e
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2024-05-23 00:25:52 -0700

    removed test code in main.js as well

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
 class Book {
 }
-let myBook = new Book();
-myBook.isbn = "123";
-myBook.price = 9.99;
-myBook.title = "Programming for Beginners";
-myBook.releaseDate = new Date(2023, 9, 8);
-console.log(myBook);
 window.onload = function () {
     let addBookButton = document.querySelector("#add-book");
     addBookButton.onclick = processBook;
